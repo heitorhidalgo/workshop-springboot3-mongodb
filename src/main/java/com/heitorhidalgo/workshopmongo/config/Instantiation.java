3378252d88707eb873e14a10f40b2065779b18f0
Heitor Hidalgo <heitorhidalgo10@hotmail.com>
2025-10-30 17:11:38 -0300

    Add comments to posts.

--- src/main/java/com/heitorhidalgo/workshopmongo/config/Instantiation.java
+++ src/main/java/com/heitorhidalgo/workshopmongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.heitorhidalgo.workshopmongo.domain.Post;
 import com.heitorhidalgo.workshopmongo.domain.User;
 import com.heitorhidalgo.workshopmongo.dto.AuthorDTO;
+import com.heitorhidalgo.workshopmongo.dto.CommentDTO;
 import com.heitorhidalgo.workshopmongo.repository.PostRepository;
 import com.heitorhidalgo.workshopmongo.repository.UserRepository;
 
@@ -39,8 +40,14 @@ public class Instantiation implements CommandLineRunner {
 		userReposiroty.saveAll(Arrays.asList(tenma, johan, anna));
 		
 		Post post1 = new Post(null, sdf.parse("30/10/2025"), "Em busca do Johan", "Estou procurando o Johan por toda a Alemanha.", new AuthorDTO(tenma));
-		Post post2 = new Post(null, sdf.parse("30/10/2025"), "Nao me recordo do passado", "Nao me lembro da minha infancia e do meu irmao Johan.", new AuthorDTO(anna));
+		Post post2 = new Post(null, sdf.parse("30/10/2025"), "Nao me recordo do passado", "Nao lembro da minha infancia e do meu irmao gemeo Johan.", new AuthorDTO(anna));
 
+		CommentDTO c1 = new CommentDTO("Estou sempre um passo a frente.", sdf.parse("30/10/2025"), new AuthorDTO(johan));
+		CommentDTO c2 = new CommentDTO("Tente se lembrar Anna.", sdf.parse("30/10/2025"), new AuthorDTO(tenma));
+		
+		post1.getComments().addAll(Arrays.asList(c1));
+		post2.getComments().addAll(Arrays.asList(c2));
+		
 		postReposiroty.saveAll(Arrays.asList(post1, post2));
 		
 		tenma.getPosts().addAll(Arrays.asList(post1));
